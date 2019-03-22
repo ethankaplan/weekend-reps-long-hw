25eecb86979054c21a14d62e298e4c4b8ec3de50
Ethan <N/A>
2019-03-22 14:34:42 -0700

    Block E done. First section done

--- js/app.js
+++ js/app.js
@@ -1,45 +1,55 @@
 {
-    // 1. declare = value
-    // 2. var = value
-    // 3. new = old
-    // 4. decalre makes the variable, assign is to give a value to a variable, define is function based
-    // 5. writing out the code you plan to do in english, helps plan
-    // 6. 80 to 20
+  // 1. declare = value
+  // 2. var = value
+  // 3. new = old
+  // 4. decalre makes the variable, assign is to give a value to a variable, define is function based
+  // 5. writing out the code you plan to do in english, helps plan
+  // 6. 80 to 20
 }
 
 {
-    let firstVariable = "Hello World";
-    firstVariable = 1;
-    let secondVariable = firstVariable;
-    secondVariable = "Hi";
-    //firstVariable is 1
-    let yourName =  "Ethan";
-    yourname = "Hello, my name is "+yourName;
+  let firstVariable = "Hello World";
+  firstVariable = 1;
+  let secondVariable = firstVariable;
+  secondVariable = "Hi";
+  //firstVariable is 1
+  let yourName = "Ethan";
+  yourname = "Hello, my name is " + yourName;
 }
 
 {
-    const a = 4;
-    const b = 53;
-    const c = 57;
-    const d = 16;
-    const e = 'Kevin';
-  
-    console.log(a < b);
-    console.log(c > d);
-    console.log('Name' == 'Name');
-    console.log(true != false);
-    console.log(false || false || false || true);
-    console.log(false == false)
-    console.log(e == 'Kevin');
-    console.log(a != b != c);
-    console.log(a == a != d);
-    console.log(48 == '48');
+  const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = "Kevin";
+
+  console.log(a < b);
+  console.log(c > d);
+  console.log("Name" == "Name");
+  console.log(true != false);
+  console.log(false || false || false || true);
+  console.log(false == false);
+  console.log(e == "Kevin");
+  console.log((a != b) != c);
+  console.log((a == a) != d);
+  console.log(48 == "48");
 }
 
 {
-    let animal = "cow";
-    if(animal=='cow'){
-        console.log("moooo");
-    }
-    else{console.log("Hey! You're not a cow!");}
+  let animal = "cow";
+  if (animal == "cow") {
+    console.log("moooo");
+  } else {
+    console.log("Hey! You're not a cow!");
+  }
+}
+
+{
+  let age = 16;
+  if (age >= 16) {
+    console.log("Here are the keys");
+  } else {
+    console.log("Sorry, you are too young");
+  }
 }
